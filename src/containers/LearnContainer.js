a6654eddd0ca0341bd067b9da2244e196cdaee7c
Alex Trzeciak <trebek08@gmail.com>
2018-10-07 22:19:56 -0700

    modify container

--- src/containers/LearnContainer.js
+++ src/containers/LearnContainer.js
@@ -1,19 +1,11 @@
 import { connect } from 'react-redux';
 import { getRestaurants } from '../actions';
-import Learn from '../components/Learn';
-import type { Dispatch } from 'redux';
-import type { Restaurant } from '../types/restaurants';
-
-type Action = { type: string };
-
-const mapDispatchToProps = (
-  dispatch: Dispatch<Action>
-): { restaurants: Array<Restaurant> } => ({
-  // $FlowFixMe
-  restaurants: () => dispatch(getRestaurants()),
-});
-
+​
+const mapDispatchToProps = (dispatch, ownProps) => ({
+  restaurants: () => dispatch(getRestaurants())
+})
+​
 export default connect(
   null,
   mapDispatchToProps
-)(Learn);
+)(LearnContainer)
