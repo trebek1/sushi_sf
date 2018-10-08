585de683048d7e97ab670eab289a4508c55f96c2
Alex Trzeciak <trebek08@gmail.com>
2018-10-07 19:50:45 -0700

    add some moc restaurants

--- src/components/Learn.js
+++ src/components/Learn.js
@@ -1,7 +1,15 @@
 /* @flow */
 
 import React from "react";
-import { AppBar, Toolbar, Typography } from "@material-ui/core";
+import {
+  AppBar,
+  CardMedia,
+  Divider,
+  List,
+  ListItem,
+  Toolbar,
+  Typography
+} from "@material-ui/core";
 
 const Learn = () => (
   <div>
@@ -12,6 +20,44 @@ const Learn = () => (
         </Typography>
       </Toolbar>
     </AppBar>
+    <List className="listOfRestaurants">
+      <ListItem container className="listItemContainer">
+        <div item className="listItem">
+          Sushi 1
+        </div>
+        <div item className="listItem">
+          <img
+            className="listImage"
+            src="https://i.postimg.cc/0yLc9Wff/sushi.png"
+          />
+        </div>
+      </ListItem>
+      <Divider />
+      <ListItem container className="listItemContainer">
+        Sushi 2
+        <img
+          className="listImage"
+          src="https://i.postimg.cc/0yLc9Wff/sushi.png"
+        />
+      </ListItem>
+      <Divider />
+      <ListItem container className="listItemContainer">
+        Sushi 3
+        <img
+          className="listImage"
+          src="https://i.postimg.cc/0yLc9Wff/sushi.png"
+        />
+      </ListItem>
+      <Divider />
+      <ListItem container className="listItemContainer">
+        Sushi 4
+        <img
+          className="listImage"
+          src="https://i.postimg.cc/0yLc9Wff/sushi.png"
+        />
+      </ListItem>
+      <Divider />
+    </List>
   </div>
 );
 
